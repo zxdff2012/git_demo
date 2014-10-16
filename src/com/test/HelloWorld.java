5ffc16f98852bc864f3f6f9c0ee15e28833b767c
zxdff2012 <zxdff2012@126.com>
2014-10-16 09:29:28 +0800

    首次提交到本地仓库

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class HelloWorld {
+ public void test() {
+	System.out.println("Hello,World!");
+}
+ public static void main(String[] args) {
+	HelloWorld hw = new HelloWorld();
+	hw.test();
+}
+}
